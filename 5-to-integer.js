733eb5a8cee97e489b6d17234852ccfecede9dd6
olonde <dennis.endolo@gmail.com>
2025-06-13 19:04:58 +0300

    update 5-to-integer.js file

--- 5-to-integer.js
+++ 5-to-integer.js
@@ -1,9 +1,7 @@
-let arg = "8p";
-
-let number = parseInt(arg);
-
-if (!isNaN(number)) {
-  console.log("My number: " + number);
-} else {
-  console.log("Not a number");
+const argument = process.argv
+const Num = parseInt(argument[2]); // Convert the argument to an integer
+if (isNaN(Num)) {
+    console.log('Not a Number');
+} else { 
+      console.log('My Number:', Num); // Output the number
 }
