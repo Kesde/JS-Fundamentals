e5aeb3d5ec4cb6a3f9e9663a54a230909cb0bcec
olonde <dennis.endolo@gmail.com>
2025-06-13 20:29:58 +0300

    update 7-multi_c.js file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,10 @@
-let i = 0;
-do {
-    console.log('C is fun');
-    i++;
+const arg = process.argv[2];
+const arg1 = parseInt(arg); // Convert the argument to an integer
+if (Number.isNaN(arg1)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let x = 0; x < arg; x++) {
+    console.log("C is fun");
+  }
+}
 
-} while (i < 2);
